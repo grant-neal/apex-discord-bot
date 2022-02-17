52f4a0e0edc1fddfa4296c155a00b6609a4395e0
Connor Sidwell <azrellion@hotmail.com>
2022-02-17 22:59:58 +0000

    First draft

--- main.go
+++ main.go
@@ -2,28 +2,116 @@ package main
 
 import (
 	"fmt"
+	"flag"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+    "strings"
+    "syscall"
+	"encoding/json"
 
+	"github.com/bwmarrin/discordgo"
 	_ "github.com/joho/godotenv/autoload"
 )
 
+// Variables used for command line parameters
+var (
+    Token string
+)
+
+func init() {
+    flag.StringVar(&Token, "t", "", "Bot Token")
+    flag.Parse()
+}
+
 func main() {
-	api_key := os.Getenv("API_KEY")
-	resp, err := http.Get("https://api.mozambiquehe.re/bridge?version=5&platform=PC&player=" + os.Args[1] + "&auth=" + api_key)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// Create a new Discord session using the provided bot token.
+    dg, err := discordgo.New("Bot " + Token)
+    if err != nil {
+        fmt.Println("error creating Discord session,", err)
+        return
+    }
 
-	defer resp.Body.Close()
+    // Register the messageCreate func as a callback for MessageCreate events.
+    dg.AddHandler(messageCreate)
 
-	body, err := ioutil.ReadAll(resp.Body)
+    // In this example, we only care about receiving message events.
+    dg.Identify.Intents = discordgo.IntentsGuildMessages
 
-	if err != nil {
-		log.Fatal(err)
-	}
+    // Open a websocket connection to Discord and begin listening.
+    err = dg.Open()
+    if err != nil {
+        fmt.Println("error opening connection,", err)
+        return
+    }
 
-	fmt.Println(string(body))
+    // Wait here until CTRL-C or other term signal is received.
+    fmt.Println("Bot is now running. Press CTRL-C to exit.")
+    sc := make(chan os.Signal, 1)
+    signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+    <-sc
+
+    // Cleanly close down the Discord session.
+    dg.Close()
+}
+
+type Character struct {
+    Name string
+	Rank Rank
+}
+
+type Rank struct {
+	RankScore string
+	RankName string
+	RankDiv string
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the authenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+    // Ignore all messages created by the bot itself
+    // This isn't required in this specific example but it's a good practice.
+    if m.Author.ID == s.State.User.ID {
+        return
+    }
+	
+	command := strings.Fields(m.Content)
+
+	fmt.Println(command[0])
+
+    if command[0] == "!apexname" {
+
+		// Lots more error handling can be done to stop abuse. But be sensible for now yeah?
+		if len(command) == 1 {
+			s.ChannelMessageSend(m.ChannelID, "No name included")
+		} else {
+			api_key := os.Getenv("API_KEY")
+			resp, err := http.Get("https://api.mozambiquehe.re/bridge?version=5&platform=PC&player=" + command[1] + "&auth=" + api_key)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			
+
+			defer resp.Body.Close()
+
+			body, err := ioutil.ReadAll(resp.Body)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			var character Character
+			json.Unmarshal([]byte(string(body)), &character)
+
+			// fmt.Println("Name: %s", "Rank: %s", character.Name, character.Rank)
+
+			// s.ChannelMessageSend(m.ChannelID, string(body))
+
+			// fmt.Println(string(body))
+		}
+    }
 }
