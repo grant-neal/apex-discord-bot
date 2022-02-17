dd97774449c650bec7ae1bba95205090b7a54dd1
sneally <grant@lero.io>
2022-02-17 13:03:04 +0000

    Basic fetch stats from api with key

--- main.go
+++ main.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 
 	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
-	val := os.Getenv("API_KEY")
-	fmt.Println(val)
+	api_key := os.Getenv("API_KEY")
+	resp, err := http.Get("https://api.mozambiquehe.re/bridge?version=5&platform=PC&player=" + os.Args[1] + "&auth=" + api_key)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(body))
 }
